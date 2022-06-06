44e1a360d8174600a28f64123f8255a7421fc015
syedmjavaid <syedmjavaid@gmail.com>
2022-06-06 12:20:50 +0100

    Get all and by id

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+
 const port = 3000;
 
 const app = express();
@@ -8,8 +10,32 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send({
-    message: "Hi team Surviving with google! Our amazing is server running!",
+  fs.readFile("./models/data/data.json", "utf8", (err, jsonString) => {
+    if (err) console.log(err);
+    try {
+      const data = JSON.parse(jsonString);
+      res.send(data);
+    } catch (err) {
+      console.log("Error parsing JSON string:", err);
+    }
+  });
+});
+
+app.get("/:id", (req, res) => {
+  const postId = req.params.id;
+  fs.readFile("./models/data/data.json", "utf8", (err, jsonString) => {
+    if (err) console.log(err);
+    try {
+      const data = JSON.parse(jsonString);
+
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].id == postId) {
+          res.send(data[i]);
+        }
+      }
+    } catch (err) {
+      console.log("Error parsing JSON string:", err);
+    }
   });
 });
 
