e089cb8dde5726d38f779ade583b640fd8617f15
syedmjavaid <syedmjavaid@gmail.com>
2022-06-07 17:56:28 +0100

    added testing

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -16,3 +16,29 @@ describe("GET /", () => {
       });
   });
 });
+
+describe("GET /:id", () => {
+  // test.only("200: Responds with correct ID", () => {
+  //   return request(app)
+  //     .get("/1")
+  //     .expect(200)
+  //     .then((res) => {
+  //       console.log(res.body.id);
+  //       expect(res.body.id).toBe(1);
+  //     });
+  // });
+  it("GET /:id", async () => {
+    const response = await request(app).get("/1");
+    console.log(response.body.id);
+    expect(response.status).toEqual(200);
+    expect(response.body.id).toEqual(1);
+  });
+  test("404: Responds with not found message", () => {
+    return request(app)
+      .get("/99999")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("404 Not Found");
+      });
+  });
+});
--- server.js
+++ server.js
@@ -8,5 +8,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/", dataRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(404).send({ msg: "404 Not Found" });
+});
 
 module.exports = app;
