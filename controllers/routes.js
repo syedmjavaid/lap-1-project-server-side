751bbf0f4cbc0746a16e14e4757e01f88be9209a
syedmjavaid <syedmjavaid@gmail.com>
2022-06-08 10:52:49 +0100

    Add profanity filter

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { send } = require("express/lib/response");
 const router = express.Router();
 const { fetchAll, fetchById, fetchPost } = require("../models/post");
+
+let Filter = require("bad-words"),
+  filter = new Filter();
 
 // GET all posts
 router.get("/", (req, res) => {
@@ -23,6 +25,8 @@ router.get("/:id", (req, res, next) => {
 // POST new posts
 router.post("/", (req, res) => {
   const newData = req.body;
+  const cleanText = filter.clean(newData.text.toString());
+  newData.text = cleanText;
   fetchPost(newData, (data) => {
     res.status(201).send({ "Added message": data, newData });
   });
