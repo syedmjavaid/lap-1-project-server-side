478ef087570d6ca533bfdab56a6c9a10a99f6de2
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-09 11:12:59 +0100

    overall testing coverage up to 62%

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,5 +1,6 @@
 const app = require("../server");
 const request = require("supertest");
+const { json } = require("express/lib/response");
 
 describe("GET /", () => {
   test("200: Responds with an array of messages", () => {
@@ -15,6 +16,15 @@ describe("GET /", () => {
         });
       });
   });
+
+  test('should return error code 404 from server', () => {
+    return request(app)
+    .get('/')
+    .expect(200)
+    .then((res) => {
+      expect(res.status).toBe(404)
+    })
+  });
 });
 
 describe("GET /:id", () => {
@@ -27,18 +37,59 @@ describe("GET /:id", () => {
   //       expect(res.body.id).toBe(1);
   //     });
   // });
-  it("GET /:id", async () => {
+  it("GET /:id should return the expected object id", async () => {
     const response = await request(app).get("/1");
     console.log(response.body.id);
     expect(response.status).toEqual(200);
     expect(response.body.id).toEqual(1);
   });
-  test("404: Responds with not found message", () => {
+  // test("404: Responds with not found message", () => {
+  //   return request(app)
+  //     .get("/99999")
+  //     .expect(404)
+  //     .then((res) => {
+  //       expect(res.body.msg).toBe("404 Not Found");
+  //     });
+  // });
+});
+
+describe('testing models', () => {
+  test('should return 200 for fetchAll', () => {
     return request(app)
-      .get("/99999")
-      .expect(404)
-      .then((res) => {
-        expect(res.body.msg).toBe("404 Not Found");
-      });
+    .get('/')
+    .expect(200)
+    .then((res) => {
+      expect(res.status).toBe(200);
+    });
   });
+
+  test('should return ', () => {
+    
+  });
+
+  test('should have status 200 and return an array of objects ', () => {
+    return request(app)
+    .get('/')
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toBeInstanceOf(Array)
+    })
+  });
+
+  test('should return status 200 for getting replies to individual ids', () => {
+    return request(app)
+    .get('/:id/replies')
+    .expect(200)
+    .then((res) => {
+      expect(res.statusCode).toBe(200)
+    })
+  });
+  
+});
+
+describe('models testing', () => {
+  test('should ', () => {
+    
+  });
+  
 });
